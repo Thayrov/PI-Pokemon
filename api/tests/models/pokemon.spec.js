cb24c34acdf38067ebaa3ea33b559b393d104457
thayrov <thayrovg@gmail.com>
2024-02-01 01:51:35 -0600

    🧪 [TEST]: Backend Routes Tests
    Implemented tests for three backend routes.

--- api/tests/models/pokemon.spec.js
+++ api/tests/models/pokemon.spec.js
@@ -1,22 +1,53 @@
-const { Pokemon, conn } = require('../../src/db.js');
-const { expect } = require('chai');
+const {Pokemon, conn} = require('../../src/config/db.config');
+const {beforeEach, afterAll, describe, test, expect} = require('@jest/globals');
+
+beforeAll(async () => {
+  await conn.sync({force: true});
+});
+
+afterAll(async () => {
+  await conn.close();
+});
 
 describe('Pokemon model', () => {
-  before(() => conn.authenticate()
-    .catch((err) => {
-      console.error('Unable to connect to the database:', err);
-    }));
-  describe('Validators', () => {
-    beforeEach(() => Pokemon.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
-        Pokemon.create({})
-          .then(() => done(new Error('It requires a valid name')))
-          .catch(() => done());
-      });
-      it('should work when its a valid name', () => {
-        Pokemon.create({ name: 'Pikachu' });
-      });
+  beforeEach(async () => {
+    await Pokemon.destroy({where: {}});
+  });
+
+  test('Should create a new Pokemon', async () => {
+    const newPokemon = await Pokemon.create({
+      name: 'Pikachu',
+      hp: 35,
+      attack: 55,
+      defense: 40,
+      special_attack: 65,
+      defense: 49,
+      special_defense: 65,
+      speed: 45,
+      height: 7,
+      weight: 69,
+      types: ['flying', 'ghost'],
+      abilities: ['lightning-rod', 'static'],
+      moves: ['thunder-shock', 'tail-whip', 'quick-attack'],
     });
+
+    expect(newPokemon).toHaveProperty('id');
+    expect(newPokemon.name).toBe('Pikachu');
+  });
+
+  test('Should update a Pokemon', async () => {
+    let pikachu = await Pokemon.create({name: 'Pikachu', hp: 35});
+    pikachu = await pikachu.update({hp: 45});
+
+    expect(pikachu.hp).toBe(45);
+  });
+
+  test('Should delete a Pokemon', async () => {
+    const pikachu = await Pokemon.create({name: 'Pikachu', hp: 35});
+    await pikachu.destroy();
+
+    const foundPokemon = await Pokemon.findByPk(pikachu.id);
+
+    expect(foundPokemon).toBeNull();
   });
 });
